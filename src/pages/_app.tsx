f84e1a283a68fcb2510e054f4b579a09ba525f13
estevam96 <marcelo.estevam15@hotmail.com>
2021-01-13 14:29:49 -0300

    fix viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,19 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from '../styles/globals'
 import theme from '../styles/theme'
+import Head from 'next/head'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,10 +42,6 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
             rel="stylesheet"
