65f38dab693a43476ca0d768a4ed4e6e3952d946
estevam96 <marcelo.estevam15@hotmail.com>
2021-02-07 14:52:34 -0300

    add textarea and emojis

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,38 @@
 import React, { useState } from 'react'
+import { Emoji, EmojiData } from 'emoji-mart'
 import Head from 'next/head'
 import {
   Button,
   Container,
   Input,
   Picture,
-  PictureWrapper
+  PictureWrapper,
+  TextArea,
+  EmojiWrapper,
+  TextAreaWrapper,
+  EmojiPicker,
+  EmojiBTN
 } from '../styles/pages/Home.page'
+import 'emoji-mart/css/emoji-mart.css'
 
 const Home: React.FC = () => {
   const [phone, setPhone] = useState('')
+  const [text, setText] = useState('')
+  const [showEmoji, setShowEmoji] = useState(false)
+  const [textFocus, setTextFocus] = useState(false)
 
   const generateLink = (): void => {
     const clearNumber = phone.replace(/([^\d])+/gim, '')
     window.open(
-      `${process.env.NEXT_PUBLIC_WHATSAPP_LINK}${clearNumber}`,
+      `${process.env.NEXT_PUBLIC_WHATSAPP_LINK}${clearNumber}&text=${text}`,
       '_blank'
     )
+  }
+
+  const handleEmoji = (emoji: EmojiData) => {
+    console.log(typeof emoji.native);
+
+    setText(text.concat(emoji.native))
   }
 
   return (
@@ -38,6 +54,40 @@ const Home: React.FC = () => {
             setPhone(e.target.value)
           }
         />
+        <TextAreaWrapper focus={textFocus}>
+          <TextArea
+            name="mensage"
+            id="mensage-txt"
+            placeholder="Mensagem (Opcional)"
+            cols={30}
+            rows={8}
+            value={text}
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+              setText(e.target.value)
+            }
+            onFocus={() => setTextFocus(true)}
+            onBlur={() => setTextFocus(false)}
+          ></TextArea>
+          <EmojiWrapper>
+            <EmojiBTN
+              emoji="grinning"
+              size={20}
+              onClick={() => setShowEmoji(!showEmoji)}
+            />
+          </EmojiWrapper>
+        </TextAreaWrapper>
+        {showEmoji ? (
+          <EmojiPicker
+            title="Pick your emoji…"
+            emoji="point_up"
+            showPreview={false}
+            emojiTooltip={false}
+            showSkinTones={false}
+            onSelect={emoji => handleEmoji(emoji)}
+          />
+        ) : (
+          <></>
+        )}
         <Button onClick={generateLink}>
           <b>Gerar link</b>
         </Button>
--- src/styles/pages/Home.page.ts
+++ src/styles/pages/Home.page.ts
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 import Image from 'next/image'
 import InputMask from 'react-input-mask'
+import { Picker, Emoji } from 'emoji-mart'
+
+interface TextAreaWrapperProps {
+  focus: boolean
+}
 
 export const Container = styled.div`
-  width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
@@ -45,7 +49,57 @@ export const Button = styled.button`
 export const PictureWrapper = styled.div`
   margin-bottom: 20px;
 `
-
 export const Picture = styled(Image)`
   margin: 200px;
 `
+export const TextAreaWrapper = styled.div<TextAreaWrapperProps>`
+  width: 20%;
+  padding: 5px;
+  margin-top: 15px;
+  border: 1px solid #bdc3c7;
+  border-radius: 10px;
+  background-color: white;
+  ${({ focus, theme }) =>
+    focus &&
+    `outline: none;
+    border: 1px solid ${theme.colors.primary};
+    box-shadow: -5px 7px 6px 0px rgba(192, 182, 182, 1);
+    -webkit-box-shadow: -5px 7px 6px 0px rgba(192, 182, 182, 1);
+    -moz-box-shadow: -5px 7px 6px 0px rgba(192, 182, 182, 1);
+   `}
+
+  @media screen and (max-width: 1024px) {
+    width: 50%;
+  }
+  @media screen and (max-width: 425px) {
+    width: 80%;
+    font-size: 0.7em;
+  }
+`
+export const EmojiBTN = styled(Emoji)`
+  background-color: transparent;
+  &:hover {
+    background-color: transparent;
+  }
+`
+export const EmojiPicker = styled(Picker)`
+  width: 100%;
+`
+export const TextArea = styled.textarea`
+  width: 100%;
+  padding: 5px;
+  border: none;
+  outline: none;
+  @media screen and (max-width: 425px) {
+    font-size: 1em;
+  }
+`
+export const EmojiWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+`
+
+TextAreaWrapper.defaultProps = {
+  focus: false
+}
